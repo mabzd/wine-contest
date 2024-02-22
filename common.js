b763141f38a64aa270569077332903dc75271dfd
Marcin <marcin@pockit.com>
2024-02-22 20:18:24 +0100

    refactor

--- common.js
+++ common.js
@@ -90,58 +90,74 @@ const Wines = [
 ];
 
 const Judges = [
-    { name: "MONI", initials: "Mo" },
-    { name: "DŻOANKA", initials: "DŻ" },
-    { name: "ADAM", initials: "A" },
-    { name: "5", initials: "5" },
-    { name: "Pyś", initials: "Pś" },
-    { name: "B̴̝́a̸͝ͅr̸̦̈́t̸̯͒o̸͈͝s̶̞͊ẑ̷̠", initials: "B̴̝́" },
-    { name: "MAJKA ♥", initials: "Ma" },
+    { 
+        name: "MONI", 
+        initials: "Mo",
+        votes: [
+            { left: "2", right: "1", winner: "2" },
+            { left: "3", right: "8", winner: "8" },
+            { left: "4", right: "7", winner: "4", leftNote: "TOPKA!" },
+            { left: "5", right: "6", winner: "5" },
+        ],
+    },
+    { 
+        name: "DŻOANKA", 
+        initials: "DŻ",
+        votes: [
+            { left: "3", right: "4", winner: "4" },
+            { left: "1", right: "7", winner: "1" },
+            { left: "8", right: "6", winner: "8" },
+            { left: "2", right: "5", winner: "2" },
+        ],
+    },
+    { 
+        name: "ADAM",
+        initials: "A",
+        votes: [
+            { left: "6", right: "2", winner: "6" },
+            { left: "7", right: "8", winner: "8" },
+            { left: "4", right: "1", winner: "1" },
+            { left: "5", right: "3", winner: "5" },
+        ],
+    },
+    { 
+        name: "5", 
+        initials: "5",
+        votes: [
+            { left: "7", right: "5", winner: "7", note: "2 zapach 1\n2 smak 2\n2 posmak 1\n6 - 4" },
+            { left: "8", right: "4", winner: "4", note: "3 zapach 2\n2 smak 3\n2 posmak 3\n7 - 8" },
+            { left: "2", right: "3", winner: "2", note: "2 zapach 2\n2 smak 2\n3 posmak 3" },
+            { left: "6", right: "1", winner: "6", note: "3 zapach 2\n2 smak 3\n2 posmak 2" },
+        ],
+    },
+    { 
+        name: "Pyś",
+        initials: "Pś",
+        votes: [
+            { left: "5", right: "8", winner: "5" },
+            { left: "1", right: "3", winner: "3", note: "ciężka decyzja" },
+            { left: "6", right: "7", winner: "7" },
+            { left: "4", right: "2", winner: "2", leftNote: "bardzo słodkie!" },
+        ],
+    },
+    { 
+        name: "B̴̝́a̸͝ͅr̸̦̈́t̸̯͒o̸͈͝s̶̞͊ẑ̷̠",
+        initials: "B̴̝́",
+        votes: [
+            { left: "3", right: "6", winner: "3" },
+            { left: "2", right: "7", winner: "2" },
+            { left: "4", right: "5", winner: "4" },
+            { left: "8", right: "1", winner: "8" },
+        ],
+    },
+    { 
+        name: "MAJKA ♥",
+        initials: "Ma",
+        votes: [
+            { left: "1", right: "5", winner: "1" },
+            { left: "6", right: "4", winner: "4" },
+            { left: "8", right: "2", winner: "2", rightNote: "♥" },
+            { left: "7", right: "3", winner: "3" },
+        ],
+    },
 ];
-
-const VotesByJudges = [
-    [
-        { left: "2", right: "1", winner: "2" },
-        { left: "3", right: "8", winner: "8" },
-        { left: "4", right: "7", winner: "4", leftNote: "TOPKA!" },
-        { left: "5", right: "6", winner: "5" },
-    ],
-    [
-        { left: "3", right: "4", winner: "4" },
-        { left: "1", right: "7", winner: "1" },
-        { left: "8", right: "6", winner: "8" },
-        { left: "2", right: "5", winner: "2" },
-    ],
-    [
-        { left: "6", right: "2", winner: "6" },
-        { left: "7", right: "8", winner: "8" },
-        { left: "4", right: "1", winner: "1" },
-        { left: "5", right: "3", winner: "5" },
-    ],
-    [
-        { left: "7", right: "5", winner: "7", note: "2 zapach 1\n2 smak 2\n2 posmak 1\n6 - 4" },
-        { left: "8", right: "4", winner: "4", note: "3 zapach 2\n2 smak 3\n2 posmak 3\n7 - 8" },
-        { left: "2", right: "3", winner: "2", note: "2 zapach 2\n2 smak 2\n3 posmak 3" },
-        { left: "6", right: "1", winner: "6", note: "3 zapach 2\n2 smak 3\n2 posmak 2" },
-    ],
-    [
-        { left: "5", right: "8", winner: "5" },
-        { left: "1", right: "3", winner: "3", note: "ciężka decyzja" },
-        { left: "6", right: "7", winner: "7" },
-        { left: "4", right: "2", winner: "2", leftNote: "bardzo słodkie!" },
-    ],
-    [
-        { left: "3", right: "6", winner: "3" },
-        { left: "2", right: "7", winner: "2" },
-        { left: "4", right: "5", winner: "4" },
-        { left: "8", right: "1", winner: "8" },
-    ],
-    [
-        { left: "1", right: "5", winner: "1" },
-        { left: "6", right: "4", winner: "4" },
-        { left: "8", right: "2", winner: "2", rightNote: "♥" },
-        { left: "7", right: "3", winner: "3" },
-    ]
-];
-
-const Votes = VotesByJudges.flatMap((j, ji) => j.map((r, ri) => { return { judge: ji, round: ri, ...r }}));
